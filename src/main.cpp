bdd491ad08e7676aee35c480c7bc5469315c957a
shadowcrafter01 <nunnboy1@gmail.com>
2026-07-21 02:57:55 -0500

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <SDL3_mixer/SDL_mixer.h>
 #include <SDL3_ttf/SDL_ttf.h>
 
+#include <Windows.h>
+
 #include <iostream>
 #include "init.hpp"
 
@@ -28,82 +30,97 @@ static Uint64 last_check;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    //metadata
+    // metadata
     if (!SDL_SetAppMetadata("Wrapler", "0.0", "com.wrapler.engine"))
     {
         return SDL_APP_FAILURE;
     }
 
-    //init sdl
+    // init sdl
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
         SDL_Log("SDL init error: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    //make window
+    // make window
+    //| SDL_WINDOW_TRANSPARENT | SDL_WINDOW_BORDERLESS | SDL_WINDOW_ALWAYS_ON_TOP
     if (!SDL_CreateWindowAndRenderer("le wrapler rawr", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer))
     {
         return SDL_APP_FAILURE;
     }
     SDL_SetRenderLogicalPresentation(renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    // SDL_SetWindowOpacity(window,0.5f);
 
-    //load texture
+    // load texture
     SDL_Surface *surface = SDL_LoadBMP("data/textures/billGates.bmp");
     texture_width = surface->w;
     texture_height = surface->h;
-    texture = SDL_CreateTextureFromSurface(renderer,surface);
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
 
-    //set window icon
+    // set window icon
     if (!SDL_SetWindowIcon(window, surface))
     {
         SDL_Log("texture no exist: %s", SDL_GetError());
     }
 
-    //init audio system
+    // init audio system
     if (!MIX_Init())
     {
         SDL_Log("Couldn't init SDL_mixer library: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
     mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
-    if (!mixer) {
+    if (!mixer)
+    {
         SDL_Log("Couldn't create mixer on default device: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    //load audio
+    // load audio
     audio = MIX_LoadAudio(mixer, "data/sounds/vine-boom.mp3", false);
-    if (!audio) {
+    if (!audio)
+    {
         SDL_Log("Couldn't load audio", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    //fps font thing
-    if (!TTF_Init()) {
+    // fps font thing
+    if (!TTF_Init())
+    {
         SDL_Log("Couldn't initialize SDL_ttf: %s\n", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-    font = TTF_OpenFont("data/fonts/sonic-hud-c-italic.ttf",12);
-    if (!font) {
+    font = TTF_OpenFont("data/fonts/sonic-hud-c-italic.ttf", 12);
+    if (!font)
+    {
         SDL_Log("Couldn't open font: %s\n", SDL_GetError());
         return SDL_APP_FAILURE;
     }
     engine = TTF_CreateRendererTextEngine(renderer);
-    if (!engine) {
+    if (!engine)
+    {
         SDL_Log("Couldn't create text engine: %s\n", SDL_GetError());
         return SDL_APP_FAILURE;
     }
     text = TTF_CreateText(engine, font, "Calculating...", 0);
-    if (!text) {
+    if (!text)
+    {
         SDL_Log("Couldn't create text: %s\n", SDL_GetError());
         return SDL_APP_FAILURE;
     }
     TTF_SetTextColor(text, 255, 255, 255, SDL_ALPHA_OPAQUE);
     last_check = SDL_GetTicks();
 
-    //what
+    // what
     SDL_Log("what");
+
+    //SDL_SetWindowShape(window, surface);
+    //SDL_PropertiesID props = SDL_GetWindowProperties(window);
+    //HWND hwnd = (HWND)SDL_GetPointerProperty(props,SDL_PROP_WINDOW_WIN32_HWND_POINTER,nullptr);
+    //LONG exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+    //SetWindowLong(hwnd,GWL_EXSTYLE,exStyle | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+    //SetWindowPos(hwnd,nullptr,0, 0, 0, 0,SWP_NOMOVE |SWP_NOSIZE |SWP_NOZORDER |SWP_FRAMECHANGED);
 
     return SDL_APP_CONTINUE;
 }
@@ -122,7 +139,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
         break;
     case SDL_EVENT_MOUSE_BUTTON_DOWN:
         // mouse down stuff here
-        MIX_PlayAudio(mixer,audio);
+        MIX_PlayAudio(mixer, audio);
         break;
     case SDL_EVENT_MOUSE_BUTTON_UP:
         // mouse up stuff here
@@ -137,33 +154,33 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
     // main render loop (runs as fast as possible)
-
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
 
-    SDL_GetMouseState(&mouseX,&mouseY);
+    SDL_GetMouseState(&mouseX, &mouseY);
 
     SDL_FRect rect;
-    rect.x=mouseX;
-    rect.y=mouseY;
-    rect.h=texture_height;
-    rect.w=texture_width;
-    SDL_RenderTexture(renderer,texture,NULL,&rect);
+    rect.x = mouseX;
+    rect.y = mouseY;
+    rect.h = texture_height;
+    rect.w = texture_width;
+    SDL_RenderTexture(renderer, texture, NULL, &rect);
 
     /* Draw a single triangle with a different color at each vertex. Center this one and make it grow and shrink. */
     /* You always draw triangles with this, but you can string triangles together to form polygons. */
     SDL_Vertex vertices[4];
     const float size = 200.0f;
     SDL_zeroa(vertices);
-    vertices[0].position.x = mouseX;//((float) WINDOW_WIDTH) / 2.0f;
-    vertices[0].position.y = mouseY;//(((float) WINDOW_HEIGHT) - size) / 2.0f;
+    vertices[0].position.x = mouseX; //((float) WINDOW_WIDTH) / 2.0f;
+    vertices[0].position.y = mouseY; //(((float) WINDOW_HEIGHT) - size) / 2.0f;
     vertices[0].color.r = 1.0f;
     vertices[0].color.a = 1.0f;
-    vertices[1].position.x = (((float) WINDOW_WIDTH) + size) / 2.0f;
-    vertices[1].position.y = (((float) WINDOW_HEIGHT) + size) / 2.0f;
+    vertices[1].position.x = (((float)WINDOW_WIDTH) + size) / 2.0f;
+    vertices[1].position.y = (((float)WINDOW_HEIGHT) + size) / 2.0f;
     vertices[1].color.g = 1.0f;
     vertices[1].color.a = 1.0f;
-    vertices[2].position.x = (((float) WINDOW_WIDTH) - size) / 2.0f;
-    vertices[2].position.y = (((float) WINDOW_HEIGHT) + size) / 2.0f;
+    vertices[2].position.x = (((float)WINDOW_WIDTH) - size) / 2.0f;
+    vertices[2].position.y = (((float)WINDOW_HEIGHT) + size) / 2.0f;
     vertices[2].color.b = 1.0f;
     vertices[2].color.a = 1.0f;
     SDL_RenderGeometry(renderer, NULL, vertices, 3, NULL, 0);
@@ -172,7 +189,8 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     int text_w = 0, text_h = 0;
     float x, y;
     Uint64 now = SDL_GetTicks();
-    if ((now - last_check) >= 100) {
+    if ((now - last_check) >= 100)
+    {
         char string[6];
         float fps = frames * 1000.0f / (now - last_check);
         SDL_snprintf(string, sizeof(string), "%.2f", fps);
@@ -188,7 +206,7 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     y = (float)(h - text_h) / 2;
     TTF_DrawRendererText(text, x, y);
 
-    //SDL_Delay(16);
+    // SDL_Delay(16);
     ++frames;
 
     SDL_RenderPresent(renderer);
