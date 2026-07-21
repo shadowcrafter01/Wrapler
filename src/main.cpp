d3d54f51a35d4644f102fc0e3b8de83b24d7336b
shadowcrafter01 <nunnboy1@gmail.com>
2026-07-21 02:09:47 -0500

    oh boy i got something started for once

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,202 @@
+#define SDL_MAIN_USE_CALLBACKS 1
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_mixer/SDL_mixer.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
 #include <iostream>
 #include "init.hpp"
 
-// entry point
-int main(int argc, char *argv[])
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+static SDL_Texture *texture = NULL;
+static int texture_width = 0;
+static int texture_height = 0;
+static float mouseX;
+static float mouseY;
+static MIX_Mixer *mixer = NULL;
+static MIX_Audio *audio = NULL;
+static MIX_Track *track = NULL;
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+static TTF_Font *font = NULL;
+static TTF_TextEngine *engine = NULL;
+static TTF_Text *text = NULL;
+static int frames;
+static Uint64 last_check;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    std::cout << "Hello, from Wrapler!\n";
-    return 0;
+    //metadata
+    if (!SDL_SetAppMetadata("Wrapler", "0.0", "com.wrapler.engine"))
+    {
+        return SDL_APP_FAILURE;
+    }
+
+    //init sdl
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_Log("SDL init error: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    //make window
+    if (!SDL_CreateWindowAndRenderer("le wrapler rawr", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer))
+    {
+        return SDL_APP_FAILURE;
+    }
+    SDL_SetRenderLogicalPresentation(renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    //load texture
+    SDL_Surface *surface = SDL_LoadBMP("data/textures/billGates.bmp");
+    texture_width = surface->w;
+    texture_height = surface->h;
+    texture = SDL_CreateTextureFromSurface(renderer,surface);
+
+    //set window icon
+    if (!SDL_SetWindowIcon(window, surface))
+    {
+        SDL_Log("texture no exist: %s", SDL_GetError());
+    }
+
+    //init audio system
+    if (!MIX_Init())
+    {
+        SDL_Log("Couldn't init SDL_mixer library: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
+    if (!mixer) {
+        SDL_Log("Couldn't create mixer on default device: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    //load audio
+    audio = MIX_LoadAudio(mixer, "data/sounds/vine-boom.mp3", false);
+    if (!audio) {
+        SDL_Log("Couldn't load audio", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    //fps font thing
+    if (!TTF_Init()) {
+        SDL_Log("Couldn't initialize SDL_ttf: %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    font = TTF_OpenFont("data/fonts/sonic-hud-c-italic.ttf",12);
+    if (!font) {
+        SDL_Log("Couldn't open font: %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    engine = TTF_CreateRendererTextEngine(renderer);
+    if (!engine) {
+        SDL_Log("Couldn't create text engine: %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    text = TTF_CreateText(engine, font, "Calculating...", 0);
+    if (!text) {
+        SDL_Log("Couldn't create text: %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    TTF_SetTextColor(text, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    last_check = SDL_GetTicks();
+
+    //what
+    SDL_Log("what");
+
+    return SDL_APP_CONTINUE;
 }
 
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    switch (event->type)
+    {
+    case SDL_EVENT_QUIT:
+        return SDL_APP_SUCCESS;
+    case SDL_EVENT_KEY_DOWN:
+        // keyboard down stuff here
+        break;
+    case SDL_EVENT_KEY_UP:
+        // keyboard up stuff here
+        break;
+    case SDL_EVENT_MOUSE_BUTTON_DOWN:
+        // mouse down stuff here
+        MIX_PlayAudio(mixer,audio);
+        break;
+    case SDL_EVENT_MOUSE_BUTTON_UP:
+        // mouse up stuff here
+        break;
+    default:
+        break;
+    }
 
+    return SDL_APP_CONTINUE;
+}
 
-//apparently a big new thing with sdl3 is that it changes the entire "entry point" thing, or at least provides a new option, may look into it or just keep doing things the old way
-//it looks hella simple now but like idk how i feel about it
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    // main render loop (runs as fast as possible)
 
+    SDL_RenderClear(renderer);
 
+    SDL_GetMouseState(&mouseX,&mouseY);
 
-//-----------
+    SDL_FRect rect;
+    rect.x=mouseX;
+    rect.y=mouseY;
+    rect.h=texture_height;
+    rect.w=texture_width;
+    SDL_RenderTexture(renderer,texture,NULL,&rect);
 
+    /* Draw a single triangle with a different color at each vertex. Center this one and make it grow and shrink. */
+    /* You always draw triangles with this, but you can string triangles together to form polygons. */
+    SDL_Vertex vertices[4];
+    const float size = 200.0f;
+    SDL_zeroa(vertices);
+    vertices[0].position.x = mouseX;//((float) WINDOW_WIDTH) / 2.0f;
+    vertices[0].position.y = mouseY;//(((float) WINDOW_HEIGHT) - size) / 2.0f;
+    vertices[0].color.r = 1.0f;
+    vertices[0].color.a = 1.0f;
+    vertices[1].position.x = (((float) WINDOW_WIDTH) + size) / 2.0f;
+    vertices[1].position.y = (((float) WINDOW_HEIGHT) + size) / 2.0f;
+    vertices[1].color.g = 1.0f;
+    vertices[1].color.a = 1.0f;
+    vertices[2].position.x = (((float) WINDOW_WIDTH) - size) / 2.0f;
+    vertices[2].position.y = (((float) WINDOW_HEIGHT) + size) / 2.0f;
+    vertices[2].color.b = 1.0f;
+    vertices[2].color.a = 1.0f;
+    SDL_RenderGeometry(renderer, NULL, vertices, 3, NULL, 0);
 
+    int w = 0, h = 0;
+    int text_w = 0, text_h = 0;
+    float x, y;
+    Uint64 now = SDL_GetTicks();
+    if ((now - last_check) >= 100) {
+        char string[6];
+        float fps = frames * 1000.0f / (now - last_check);
+        SDL_snprintf(string, sizeof(string), "%.2f", fps);
+        TTF_SetTextString(text, string, 0);
 
-/*
- * This example code creates an SDL window and renderer, and then clears the
- * window to a different color every frame, so you'll effectively get a window
- * that's smoothly fading between colors.
- *
- * This code is public domain. Feel free to use it for any purpose!
- */
+        /* Reset our counters for the next cycle */
+        last_check = now;
+        frames = 0;
+    }
+    SDL_GetRenderOutputSize(renderer, &w, &h);
+    TTF_GetTextSize(text, &text_w, &text_h);
+    x = (float)(w - text_w) / 2;
+    y = (float)(h - text_h) / 2;
+    TTF_DrawRendererText(text, x, y);
 
-//#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
-//#include <SDL3/SDL.h>
-//#include <SDL3/SDL_main.h>
-//
-///* We will use this renderer to draw into this window every frame. */
-//static SDL_Window *window = NULL;
-//static SDL_Renderer *renderer = NULL;
-//
-///* This function runs once at startup. */
-//SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
-//{
-//    SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
-//
-//    if (!SDL_Init(SDL_INIT_VIDEO)) {
-//        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
-//        return SDL_APP_FAILURE;
-//    }
-//
-//    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
-//        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
-//        return SDL_APP_FAILURE;
-//    }
-//    SDL_SetRenderLogicalPresentation(renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
-//
-//    return SDL_APP_CONTINUE;  /* carry on with the program! */
-//}
-//
-///* This function runs when a new event (mouse input, keypresses, etc) occurs. */
-//SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-//{
-//    if (event->type == SDL_EVENT_QUIT) {
-//        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
-//    }
-//    return SDL_APP_CONTINUE;  /* carry on with the program! */
-//}
-//
-///* This function runs once per frame, and is the heart of the program. */
-//SDL_AppResult SDL_AppIterate(void *appstate)
-//{
-//    const double now = ((double)SDL_GetTicks()) / 1000.0;  /* convert from milliseconds to seconds. */
-//    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
-//    const float red = (float) (0.5 + 0.5 * SDL_sin(now));
-//    const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-//    const float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-//    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);  /* new color, full alpha. */
-//
-//    /* clear the window to the draw color. */
-//    SDL_RenderClear(renderer);
-//
-//    /* put the newly-cleared rendering on the screen. */
-//    SDL_RenderPresent(renderer);
-//
-//    return SDL_APP_CONTINUE;  /* carry on with the program! */
-//}
-//
-///* This function runs once at shutdown. */
-//void SDL_AppQuit(void *appstate, SDL_AppResult result)
-//{
-//    /* SDL will clean up the window/renderer for us. */
-//}
+    //SDL_Delay(16);
+    ++frames;
+
+    SDL_RenderPresent(renderer);
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    SDL_DestroyTexture(texture);
+    // end things
+}
