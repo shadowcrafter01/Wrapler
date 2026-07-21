67ad8cdc2ef93ff0d8414c93d5bbdb90bfc93e0a
shadowcrafter01 <nunnboy1@gmail.com>
2026-07-21 02:59:27 -0500

    move shit into the header to make sure it compiles

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,10 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-#include <SDL3_mixer/SDL_mixer.h>
-#include <SDL3_ttf/SDL_ttf.h>
-
-#include <Windows.h>
-
-#include <iostream>
 #include "init.hpp"
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
-static SDL_Texture *texture = NULL;
-static int texture_width = 0;
-static int texture_height = 0;
-static float mouseX;
-static float mouseY;
-static MIX_Mixer *mixer = NULL;
-static MIX_Audio *audio = NULL;
-static MIX_Track *track = NULL;
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
-static TTF_Font *font = NULL;
-static TTF_TextEngine *engine = NULL;
-static TTF_Text *text = NULL;
-static int frames;
-static Uint64 last_check;
+
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
